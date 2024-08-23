30109911791a8f849a72c374a3d88aa17cb18588
Rutger van Bergen <rbergen@xs4all.nl>
2024-08-23 12:53:18 +0200

    Missed one

--- src/MixAssembler/Finding/AssemblyFinding.cs
+++ src/MixAssembler/Finding/AssemblyFinding.cs
@@ -5,11 +5,11 @@ namespace MixAssembler.Finding
 
 	public abstract class AssemblyFinding(Severity severity, int lineNumber, LineSection section, int startCharIndex, int length)
   {
-		public int Length { get; private set; } = length;
-		public int LineNumber { get; private set; } = lineNumber;
-		public LineSection LineSection { get; private set; } = section;
-		public Severity Severity { get; private set; } = severity;
-		public int StartCharIndex { get; private set; } = startCharIndex;
+		public int Length => length;
+		public int LineNumber => lineNumber;
+		public LineSection LineSection => section;
+		public Severity Severity => severity;
+		public int StartCharIndex => startCharIndex;
 
 		public const int NoNumber = int.MinValue;
 
